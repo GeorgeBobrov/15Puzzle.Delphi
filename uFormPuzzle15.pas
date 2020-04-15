9b2f9cb0e8f672c13322a3914a8dd893b89e4123
George Bobrov <skivvvvy@gmail.com>
2020-04-15 23:23:06 +0300

    Change Shuffle method (by moving tiles)

--- uFormPuzzle15.pas
+++ uFormPuzzle15.pas
@@ -61,6 +61,7 @@ type
     ButtonPuzzleMatched: TButton;
     ButtonTimeOver: TButton;
     CheckBoxPl1: TCheckBox;
+    SpeedButtonEffects: TSpeedButton;
     procedure CreateTiles;
     procedure ButtonMenuClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
@@ -82,6 +83,7 @@ type
     procedure TimerCloseTimer(Sender: TObject);
     procedure ButtonPuzzleMatchedClick(Sender: TObject);
     procedure ButtonTimeOverClick(Sender: TObject);
+    procedure SpeedButtonEffectsClick(Sender: TObject);
     type
       TMode = (Game, GameOver, JustShuffled, PuzzleMatched);
   private
@@ -91,15 +93,6 @@ type
     procedure SetMode(const Value: TMode);
     procedure SetMaxTime;
     procedure SetBase(const Value: integer);
-    procedure AnimatePlaceTilesFast;
-    procedure AnimatePlaceTilesSlow;
-    procedure AnimateTilesDisappeare;
-    procedure AnimatePrepareBeforePlace;
-    procedure AnimateBaseNotChanged;
-    procedure AnimateTimeRunningOut;
-    procedure AnimatePuzzleMatched;
-    procedure AnimateTimeOver;
-    procedure AnimateNormalizeTilesColor;
   public
     { Public declarations }
     Tiles: array of TRectangle;
@@ -110,14 +103,30 @@ type
     TileFillNormalColor, TileFillNormalColor1: TColor;
     LastResizeTime: TDateTime;
     ClosingAnimation: Boolean;
+    WaitAnimationEnd: Boolean;
 
     property Base: integer read FBase write SetBase;
     property Mode: TMode read FMode write SetMode;
 
+    function TryMoveTile(TilePosition: integer; MoveAniDuration: single): Boolean;
+    procedure AnimateMoveTile(ATile: TRectangle; MoveAniDuration: single);
+
+    function CheckCanPuzzleMatch: Boolean;
+    procedure CheckPuzzleMatched;
+
     procedure CalcConsts;
 
-    procedure MoveTile(ATile: TRectangle; NewRow, NewCol: Word);
-    procedure CheckPuzzleMatched;
+    procedure AnimatePlaceTilesFast;
+    procedure AnimatePlaceTilesSlow;
+    procedure AnimateTilesDisappeare;
+    procedure AnimatePrepareBeforePlace;
+    procedure AnimateBaseNotChanged;
+    procedure AnimateTimeRunningOut;
+    procedure AnimatePuzzleMatched;
+    procedure AnimateTimeOver;
+    procedure AnimateNormalizeTilesColor;
+    procedure StartBlinkShuffle;
+    procedure StopBlinkShuffle;
   end;
 
 var
@@ -127,6 +136,12 @@ implementation
 
 {$R *.fmx}
 {$R *.LgXhdpiPh.fmx ANDROID}
+{$R *.Windows.fmx MSWINDOWS}
+
+const
+  MaxMoveAniDuration = 0.15;
+  MinMoveAniDuration = 0.001;
+
 
 procedure TFormPuzzle15.FormCreate(Sender: TObject);
 begin
@@ -136,6 +151,7 @@ begin
 
   TileFillNormalColor := Tile1.Fill.Gradient.InterpolateColor(0);
   TileFillNormalColor1 := Tile1.Fill.Gradient.InterpolateColor(1);
+
   Base := 4;
 end;
 
@@ -149,6 +165,8 @@ begin
   FMode := Value;
   TimerTime.Enabled := (FMode = Game);
 end;
+
+
 
 
 
@@ -211,6 +229,7 @@ begin
   Tile1.TagFloat := 0;     //Actual number of Tile
 
   Tile1.Opacity := 0;
+  Tile1.Visible := true;
 
 
   for i := 0 to Length(Tiles) - 1 do
@@ -276,16 +295,29 @@ end;
 procedure TFormPuzzle15.TileMouseDown(Sender: TObject; Button: TMouseButton;
   Shift: TShiftState; X, Y: Single);
 var
-  RowPressed, ColPressed: Word;
-  Row, Col, RowNoTile, ColNoTile, RowToMove, ColToMove: integer;
   SenderTile: TRectangle absolute Sender;
+  WasMoved: Boolean;
 begin
-
   if (Mode = JustShuffled) then
     Mode := Game;
 
-  DivMod(SenderTile.Tag, Base, RowPressed, ColPressed);
+  WasMoved := TryMoveTile(SenderTile.Tag, MaxMoveAniDuration);
 
+  if WasMoved then
+    CheckPuzzleMatched;
+end;
+
+
+function TFormPuzzle15.TryMoveTile(TilePosition: integer; MoveAniDuration: single): Boolean;
+var
+  RowPressed, ColPressed: Word;
+  Row, Col, RowNoTile, ColNoTile, RowToMove, ColToMove: integer;
+  WasMoved: Boolean;
+  NewPosition: integer;
+begin
+  DivMod(TilePosition, Base, RowPressed, ColPressed);
+
+  WasMoved := false;
   for Row := 0 to Base - 1 do
     if (Tiles[ind(Row, ColPressed)] = nil) then
     begin
@@ -294,23 +326,32 @@ begin
       if (RowNoTile > RowPressed) then
         for RowToMove := RowNoTile - 1 downto RowPressed do
         begin
-          Tiles[ind(RowToMove + 1 , ColPressed)] := Tiles[ind(RowToMove , ColPressed)];
+          NewPosition := ind(RowToMove + 1 , ColPressed);
+          Tiles[NewPosition] := Tiles[ind(RowToMove , ColPressed)];
+          Tiles[NewPosition].Tag := NewPosition;
           Tiles[ind(RowToMove , ColPressed)] := nil;
-          MoveTile(Tiles[ind(RowToMove + 1 , ColPressed)], RowToMove + 1, ColPressed);
+
+          AnimateMoveTile(Tiles[NewPosition], MoveAniDuration);
+
+          WasMoved := true;
         end;
 
       if (RowPressed > RowNoTile) then
         for RowToMove := RowNoTile + 1 to RowPressed do
         begin
-          Tiles[ind(RowToMove - 1 , ColPressed)] := Tiles[ind(RowToMove , ColPressed)];
+          NewPosition := ind(RowToMove - 1 , ColPressed);
+          Tiles[NewPosition] := Tiles[ind(RowToMove , ColPressed)];
+          Tiles[NewPosition].Tag := NewPosition;
           Tiles[ind(RowToMove , ColPressed)] := nil;
-          MoveTile(Tiles[ind(RowToMove - 1 , ColPressed)], RowToMove - 1, ColPressed);
+
+          AnimateMoveTile(Tiles[NewPosition], MoveAniDuration);
+
+          WasMoved := true;
         end;
 
-      Exit;
     end;
 
-
+  if not WasMoved then
   for Col := 0 to Base - 1 do
     if (Tiles[ind(RowPressed , Col)] = nil) then
     begin
@@ -319,40 +360,64 @@ begin
       if (ColNoTile > ColPressed) then
         for ColToMove := ColNoTile - 1 downto ColPressed do
         begin
-          Tiles[ind(RowPressed , ColToMove + 1)] := Tiles[ind(RowPressed , ColToMove)];
+          NewPosition := ind(RowPressed , ColToMove + 1);
+          Tiles[NewPosition] := Tiles[ind(RowPressed , ColToMove)];
+          Tiles[NewPosition].Tag := NewPosition;
           Tiles[ind(RowPressed , ColToMove)] := nil;
-          MoveTile(Tiles[ind(RowPressed , ColToMove + 1)], RowPressed, ColToMove + 1);
+
+          AnimateMoveTile(Tiles[NewPosition], MoveAniDuration);
+
+          WasMoved := true;
         end;
 
       if (ColPressed > ColNoTile) then
         for ColToMove := ColNoTile + 1 to ColPressed do
         begin
-          Tiles[ind(RowPressed , ColToMove - 1)] := Tiles[ind(RowPressed , ColToMove)];
+          NewPosition := ind(RowPressed , ColToMove - 1);
+          Tiles[NewPosition] := Tiles[ind(RowPressed , ColToMove)];
+          Tiles[NewPosition].Tag := NewPosition;
           Tiles[ind(RowPressed , ColToMove)] := nil;
-          MoveTile(Tiles[ind(RowPressed , ColToMove - 1)], RowPressed, ColToMove - 1);
+
+          AnimateMoveTile(Tiles[NewPosition], MoveAniDuration);
+
+          WasMoved := true;
         end;
 
     end;
 
+  Result := WasMoved;
 end;
 
 
-
-
-procedure TFormPuzzle15.MoveTile(ATile: TRectangle; NewRow, NewCol: Word);
+procedure TFormPuzzle15.AnimateMoveTile(ATile: TRectangle; MoveAniDuration: single);
 var
+  NewRow, NewCol: Word;
   X, Y : Integer;
+  Duration: single;
 begin
-  ATile.Tag := NewRow * Base + NewCol;
+  DivMod(ATile.Tag, Base, NewRow, NewCol);
+
   X := SpaceX + Round(NewCol * (ATile.Width * ATile.Scale.X + TileSpacing));
   Y := SpaceY + Round(NewRow * (ATile.Height * ATile.Scale.Y + TileSpacing));
 
-  ATile.AnimateFloatDelay('Position.X', X,
-    0.15, 0, TAnimationType.Out, TInterpolationType.Exponential);
-  ATile.AnimateFloatDelay('Position.Y', Y,
-    0.15, 0, TAnimationType.Out, TInterpolationType.Exponential);
+  if MoveAniDuration > 0 then
+  begin
+    ATile.AnimateFloatDelay('Position.X', X,
+      MoveAniDuration, 0, TAnimationType.Out, TInterpolationType.Exponential);
 
-  CheckPuzzleMatched;
+    if WaitAnimationEnd then
+      ATile.AnimateFloatWait('Position.Y', Y,
+        MoveAniDuration, TAnimationType.Out, TInterpolationType.Exponential)
+    else
+      ATile.AnimateFloatDelay('Position.Y', Y,
+        MoveAniDuration, 0, TAnimationType.Out, TInterpolationType.Exponential);
+  end
+  else
+  begin
+    ATile.Position.X := X;
+    ATile.Position.Y := Y;
+  end;
+
 end;
 
 
@@ -398,84 +463,90 @@ end;
 
 
 
+
+
+
+
+
 procedure TFormPuzzle15.ButtonShuffleClick(Sender: TObject);
 var
   TilesOld: array of TRectangle;
-  i, NewI, j : Integer;
+  i, NewI : Integer;
   NewRow, NewCol: Word;
   X, Y : Integer;
-  inv, iValue, jValue: Integer;
+  MoveCount: Integer;
+  MoveAniDuration: Single;
 begin
-  SetLength(TilesOld, Length(Tiles));
-  for i := 0 to Length(Tiles) - 1 do
-  begin
-    TilesOld[i] := Tiles[i];
-    Tiles[i] := nil;
-  end;
+//  SetLength(TilesOld, Length(Tiles));
+//  for i := 0 to Length(Tiles) - 1 do
+//  begin
+//    TilesOld[i] := Tiles[i];
+//    Tiles[i] := nil;
+//  end;
+//
+//  for i := 0 to Length(Tiles) - 1 do
+//    if (TilesOld[i] <> nil) then
+//    repeat
+//      newI := Random(Length(Tiles));
+//      if (Tiles[NewI] = nil) then
+//      begin
+//        Tiles[NewI] := TilesOld[i];
+//        Tiles[NewI].Tag := NewI;
+//        Break;
+//      end;
+//
+//    until false;
+//
+//
+//  for i := 0 to Length(Tiles) - 1 do
+//    if (Tiles[i] <> nil) then
+//    begin
+//      DivMod(Tiles[i].Tag, Base, NewRow, NewCol);
+//
+//      X := SpaceX + Round(NewCol * (Tiles[i].Width * Tiles[i].Scale.X + TileSpacing));
+//      Y := SpaceY + Round(NewRow * (Tiles[i].Height * Tiles[i].Scale.Y + TileSpacing));
+//
+//      Tiles[i].AnimateFloatDelay('Position.X', X,
+//        0.4, 0, TAnimationType.Out, TInterpolationType.Exponential );
+//      Tiles[i].AnimateFloatDelay('Position.Y', Y,
+//        0.4, 0.01 * i, TAnimationType.Out, TInterpolationType.Exponential );
+//    end;
 
-  for i := 0 to Length(Tiles) - 1 do
-    if (TilesOld[i] <> nil) then
+  WaitAnimationEnd := true;
+  MoveCount := Length(Tiles) * Length(Tiles);
+  for i := 1 to MoveCount do
+  begin
+    if i <= 10 then
+      MoveAniDuration := MinMoveAniDuration + (MaxMoveAniDuration *
+        (1 - (i / 10)));
+
+    if i >= MoveCount - 10 then
+      MoveAniDuration := MinMoveAniDuration + (MaxMoveAniDuration *
+        (1 - ((MoveCount - i) / 10)));
+
+    if (i > 20) and (i < MoveCount - 20) then
+      if (i mod 10) = 0 then
+        MoveAniDuration := MinMoveAniDuration
+      else
+        MoveAniDuration := 0;
+
     repeat
       newI := Random(Length(Tiles));
-      if (Tiles[NewI] = nil) then
-      begin
-        Tiles[NewI] := TilesOld[i];
-        Tiles[NewI].Tag := NewI;
-        Break;
-      end;
+    until TryMoveTile(newI, MoveAniDuration);
+  end;
+  WaitAnimationEnd := false;
 
-    until false;
-
-  for i := 0 to Length(Tiles) - 1 do
-    if (Tiles[i] <> nil) then
-    begin
-      DivMod(Tiles[i].Tag, Base, NewRow, NewCol);
-
-      X := SpaceX + Round(NewCol * (Tiles[i].Width * Tiles[i].Scale.X + TileSpacing));
-      Y := SpaceY + Round(NewRow * (Tiles[i].Height * Tiles[i].Scale.Y + TileSpacing));
-
-      Tiles[i].AnimateFloatDelay('Position.X', X,
-        0.4, 0, TAnimationType.Out, TInterpolationType.Exponential );
-      Tiles[i].AnimateFloatDelay('Position.Y', Y,
-        0.4, 0.01 * i, TAnimationType.Out, TInterpolationType.Exponential );
-    end;
 
   SetMaxTime;
 
-  ShuffleGloomEffectAni.Enabled := false;
-//  ShuffleGloomEffect.Enabled := true;
-  ShuffleGloomEffect.BaseIntensity := 0.5;
-  ShuffleGloomEffect.BaseSaturation := 0.5;
-  ShuffleGloomEffect.GloomIntensity := 0.5;
-  ShuffleGloomEffect.GloomSaturation := 0.05;
+  StopBlinkShuffle;
 
-
-  inv := 0;
-  for i := 0 to Length(Tiles) - 1 do
-    if (Tiles[i] <> nil) then
-      for j := 0 to i - 1 do
-      begin
-        iValue := Tiles[i].Tag + 1;
-
-        if (Tiles[j] = nil) then
-          jValue := 0
-        else
-          jValue := Tiles[j].Tag + 1;
-
-        if (jValue > iValue) then
-          Inc(inv);
-      end;
-
-  for i := 0 to Length(Tiles) - 1 do
-    if (Tiles[i] = nil) then
-      Inc(inv, (i div 4) + 1);
-
-  if Odd(inv) then
-  begin
-    TimerReShuffle.Interval := 400 + 10 * Length(Tiles);
-    TimerReShuffle.Enabled := true;
-    exit;
-  end;
+//  if not CheckCanPuzzleMatch then
+//  begin
+//    TimerReShuffle.Interval := 400 + 10 * Length(Tiles);
+//    TimerReShuffle.Enabled := true;
+//    exit;
+//  end;
 
 
   Mode := JustShuffled;
@@ -483,6 +554,56 @@ begin
   CheckPuzzleMatched;
 end;
 
+
+
+
+function TFormPuzzle15.CheckCanPuzzleMatch: Boolean;
+var
+  i, j: Integer;
+  iValue, jValue: Integer;
+  inv: Integer;
+begin
+  inv := 0;
+  for i := 0 to Length(Tiles) - 1 do
+    if (Tiles[i] <> nil) then
+      for j := 0 to i - 1 do
+      begin
+        iValue := Tiles[i].Tag + 1;
+        if (Tiles[j] = nil) then
+          jValue := 0
+        else
+          jValue := Tiles[j].Tag + 1;
+        if (jValue > iValue) then
+          Inc(inv);
+      end;
+  for i := 0 to Length(Tiles) - 1 do
+    if (Tiles[i] = nil) then
+      Inc(inv, (i div Base) + 1);
+
+  Result := not Odd(inv);
+end;
+
+
+
+procedure TFormPuzzle15.StartBlinkShuffle;
+begin
+  ShuffleGloomEffect.BaseIntensity := 1;
+  ShuffleGloomEffect.BaseSaturation := 1;
+  ShuffleGloomEffect.GloomIntensity := 0;
+  ShuffleGloomEffect.GloomSaturation := 0;
+  //    ShuffleGloomEffect.Enabled := false;
+  ShuffleGloomEffectAni.Enabled := true;
+end;
+
+procedure TFormPuzzle15.StopBlinkShuffle;
+begin
+  ShuffleGloomEffectAni.Enabled := false;
+  //  ShuffleGloomEffect.Enabled := true;
+  ShuffleGloomEffect.BaseIntensity := 0.5;
+  ShuffleGloomEffect.BaseSaturation := 0.5;
+  ShuffleGloomEffect.GloomIntensity := 0.5;
+  ShuffleGloomEffect.GloomSaturation := 0.05;
+end;
 
 procedure TFormPuzzle15.SetMaxTime;
 var
@@ -824,9 +945,11 @@ end;
 
 
 
-
-
-
+procedure TFormPuzzle15.SpeedButtonEffectsClick(Sender: TObject);
+begin
+  PanelDebug.Visible := not PanelDebug.Visible;
+  SpeedButtonEffects.IsPressed := PanelDebug.Visible;
+end;
 
 
 
@@ -844,14 +967,7 @@ begin
     Mode := GameOver;
 
     AnimateTimeOver;
-
-    ShuffleGloomEffect.BaseIntensity := 1;
-    ShuffleGloomEffect.BaseSaturation := 1;
-    ShuffleGloomEffect.GloomIntensity := 0;
-    ShuffleGloomEffect.GloomSaturation := 0;
-
-//    ShuffleGloomEffect.Enabled := false;
-    ShuffleGloomEffectAni.Enabled := true;
+    StartBlinkShuffle;
 
     exit;
   end;
@@ -859,6 +975,8 @@ begin
   if (TextTime.Tag <= 10) then
     AnimateTimeRunningOut;
 end;
+
+
 
 
 procedure TFormPuzzle15.ButtonCloseClick(Sender: TObject);
